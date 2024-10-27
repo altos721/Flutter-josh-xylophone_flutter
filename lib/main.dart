c2e96bc9ddc42dffa9be50176282bbe5f34c9a41
altos721 <altos721@gmail.com>
2024-10-27 19:50:23 +0800

    Add all the sounds

--- lib/main.dart
+++ lib/main.dart
@@ -62,21 +62,44 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AssetsAudioPlayer();
+    player.open(
+      Audio('assets/note$soundNumber.wav'),
+    );
+  }
+
+  buildKey(Color color, int ID) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playSound(ID);
+          },
+          child: Text(""),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(
-            child: TextButton(
-              onPressed: () {
-                final player = AssetsAudioPlayer();
-                player.open(
-                  Audio('assets/note1.wav'),
-                );
-              },
-              child: Text("data"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
+            ],
           ),
         ),
       ),
