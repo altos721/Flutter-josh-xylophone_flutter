16d2e3f0bdc4b5613f1222742efb3cc88e2a9e57
altos721 <altos721@gmail.com>
2024-10-27 18:43:17 +0800

    Add initial codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
-void main() => runApp(XylophoneApp());
+// void main() => runApp(XylophoneApp());
+//
+// class XylophoneApp extends StatelessWidget {
+//   void playSound(int ID) {
+//     final player = AssetsAudioPlayer();
+//     player.open(
+//       Audio('assets/note$ID.wav'),
+//     );
+//   }
+//
+//   Expanded buildKey(Color color, int key) {
+//     return Expanded(
+//       child: TextButton(
+//         style: TextButton.styleFrom(
+//             shape:
+//                 const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+//             backgroundColor: color),
+//         onPressed: () {
+//           playSound(key);
+//         },
+//         child: Text(""),
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.black,
+//         body: SafeArea(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: <Widget>[
+//               buildKey(Colors.purple, 1),
+//               buildKey(Colors.indigo, 2),
+//               buildKey(Colors.blue, 3),
+//               buildKey(Colors.green, 4),
+//               buildKey(Colors.yellow, 5),
+//               buildKey(Colors.red, 6),
+//               buildKey(Colors.orange, 7),
+//             ],
+//           ),
+//         ),
+//       ),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:assets_audio_player/assets_audio_player.dart';
+// import 'dart:async';
+//
+void main() {
+  return runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -8,9 +67,20 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio('assets/note1.wav'),
+                );
+              },
+              child: Text("data"),
+            ),
+          ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
